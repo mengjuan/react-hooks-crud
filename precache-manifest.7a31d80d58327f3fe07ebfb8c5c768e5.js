33a0f49fda7a548e61fb9b87062813ad993ac838
Mona Hu <yezimengjuan@126.com>
2020-09-13 12:17:22 +0800

    Updates

---
+++ precache-manifest.7a31d80d58327f3fe07ebfb8c5c768e5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e44a4a0cee795cb0e5503248d94515fc",
+    "url": "/react-hooks-crud/index.html"
+  },
+  {
+    "revision": "7010320e054bea6966bf",
+    "url": "/react-hooks-crud/static/css/main.9447384d.chunk.css"
+  },
+  {
+    "revision": "a60d827b108dfadcabea",
+    "url": "/react-hooks-crud/static/js/2.c2b9b7cf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-hooks-crud/static/js/2.c2b9b7cf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7010320e054bea6966bf",
+    "url": "/react-hooks-crud/static/js/main.7f20375c.chunk.js"
+  },
+  {
+    "revision": "f47402e280ebaaa8c76d",
+    "url": "/react-hooks-crud/static/js/runtime-main.20b69564.js"
+  }
+]);
